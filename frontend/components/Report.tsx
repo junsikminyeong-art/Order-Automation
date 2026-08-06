ee964071e535cf190426efc70af7a38d8b473f84
junsikminyeong-art <junsikminyeong-art@github.com>
2026-08-06 14:06:23 +0900

    Report.tsx 타입 에러 수정

--- frontend/components/Report.tsx
+++ frontend/components/Report.tsx
@@ -125,7 +125,7 @@ export default function Report({ results, growthFactor, orderMonths, onClose }: 
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="color" />
                 <YAxis />
-                <Tooltip formatter={(v: number) => v.toLocaleString()} />
+                <Tooltip formatter={(v) => Number(v).toLocaleString()} />
                 <Legend />
                 <Bar dataKey="판매합계" fill="#93c5fd" radius={[4, 4, 0, 0]} />
                 <Bar dataKey="발주합계" fill="#3b82f6" radius={[4, 4, 0, 0]} />
